a9d41bb928ff47f429c2ba8a7578ad9bd476a757
ThePlugJumbo <chnxcx@gmail.com>
2022-11-26 16:30:05 -0600

    added header and footer

---
+++ components/navigation/footer.js
@@ -0,0 +1,9 @@
+export default function Footer () {
+    return <footer className="px-3 bg-gray-100">
+        <div className="max-w-screen-xl mx-auto py-8 text-center">
+            <p className="text-gray-900">
+                OpenOps Open Source
+            </p>
+        </div>
+    </footer>
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+
+import Header from "../components/navigation/header.js"
+import Footer from "../components/navigation/footer.js"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <div className="relative min-h-screen flex flex-col justify-between bg-white">
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+    </div>
 }
 
 export default MyApp
